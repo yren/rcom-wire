f5ea5543581d45389bccadca28d7c041c19d3138
Yufei Ren <yufei.ren@thomsonreuters.com>
2017-08-09 00:41:03 +0800

    minor change

--- controllers/basecontroller.js
+++ controllers/basecontroller.js
@@ -1,8 +1,19 @@
 'use strict'
 
-var restify = require("restify");
+var restify = require('restify'),
+  _ = require('underscore');
 
 function BaseController() {
   this.actions = [];
   this.server = null;
+}
+
+BaseController.prototype.setUpActions = function(app, sw) {
+  this.server = app;
+  _.each(this.actions, function(act) { 
+    var method = act['spec']['method'];
+    console.log('Setting up auto-doc for (', method, ') - ', act['spec']['nickname']);
+    sw['add' + method](act);
+    app[method.toLowerCase()](act['spec']['path'], act['action']);
+  });
 }
