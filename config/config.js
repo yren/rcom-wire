9ada1fb741f3ef44176a330025cb4ab151f88a6f
Yufei Ren <yufei.ren@thomsonreuters.com>
2017-08-09 14:58:38 +0800

    minor change

---
+++ config/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  server: {
+    name: 'Rcom Wire API',
+    version: '0.0.1',
+    port: 3000
+  },
+  memcached: {
+    host: '127.0.0.1:11211'
+  },
+  spotlight: {
+    host: 'http://apiservice.reuters.com'
+  },
+  proxy: {
+    host: 'http://10.90.7.56:3171/'
+  }
+}
